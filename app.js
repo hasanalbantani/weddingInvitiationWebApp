8b8e6eb549134dad0fbf8579909060625a4cef9b
Hasan Al Bantani <86176675+hasanalbantani@users.noreply.github.com>
2021-06-20 12:19:00 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ function openMenu(){
   var x = document.querySelector('.link');
   // x.style.display = 'none';
   if(x){
+    x.style.display = 'block';
     x.classList.add('overlay');
-  }else{
+    // x.style.width = '250px';
+  }
+}
+function closeMenu(){
+  var x = document.querySelector('.link');
+  if(x){
+    x.style.display = 'none';
     x.classList.remove('overlay');
   }
 }
+
